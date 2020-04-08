650e1fdc0bb6edff94021f3d27ed238d8afd0b3e
jgx <jules.gioux@gmail.com>
2020-04-08 12:24:24 +0200

    favicon, nétoyage code, netoyage fichiers, détails de mise en page

--- assets/js/scriptNavBar.js
+++ assets/js/scriptNavBar.js
@@ -1,15 +1,3 @@
-// window.onscroll = function() {myFunction()};
-    
-// var navbar = document.getElementById("myTopnav");
-// var sticky = navbar.offsetTop;
-
-// function myFunction() {
-//   if (window.pageYOffset >= sticky) {
-//     navbar.classList.add("sticky")
-//   } else {
-//     navbar.classList.remove("sticky");
-//   }
-// }
 function respNavBar() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
